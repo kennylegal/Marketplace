0f87e177acb3f806dde25678037a000d2048d5ab
Your Name <you@example.com>
2020-10-17 13:10:35 +0100

    changes

--- services/view.py
+++ services/view.py
@@ -66,7 +66,7 @@ def __update_if_post_method(context, business_id: int, request):
 
 
 @login_required(login_url='login')
-def update(request):
+def update(request, business_id):
     business_id = request.user.businessowner.id
     owner = BusinessOwner.objects.get(id=business_id)
     context = {
@@ -109,7 +109,7 @@ def __update_if_method_is_post(context, customer_name, request):
 
 
 @login_required(login_url='login')
-def update_customer(request):
+def update_customer(request, customer_name):
     customer_name = request.user.customer.name
     customer = get_object_or_404(Customer, name=customer_name)
     context = {
